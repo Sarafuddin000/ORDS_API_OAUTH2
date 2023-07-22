6a30ff56682b36803d10d5d03176541128884e79
Saraf Uddin <39001823+Sarafuddin000@users.noreply.github.com>
2023-07-22 16:51:51 +0600

    Update and rename ORDS API OAUTH2.txt to ORDS API OAUTH2.sql

--- ORDS API OAUTH2.sql
+++ ORDS API OAUTH2.sql
@@ -1,17 +1,17 @@
-Fist thing first
+--Fist thing first
 
-1. Create Role for API User
+--1. Create Role for API User
         SQL Workshop    >   RESTful Services\Roles\Role Definition
 
-2. Create Privileges for API User
+--2. Create Privileges for API User
         SQL Workshop    >   RESTful Services\Privileges\Privilege Definition
 
-3. Now Create API Module 
+--3. Now Create API Module 
         SQL Workshop    >   RESTful Services\Modules
 
-4. You can check your created API link for anywhere, its public
+--4. You can check your created API link for anywhere, its public
 
-5. To prevent public access you need to create client to access API
+--5. To prevent public access you need to create client to access API
 
         begin
             oauth.create_client(
@@ -23,7 +23,7 @@ Fist thing first
         commit;
         end;
 
-6. Now you need to grant already created role for this client  
+--6. Now you need to grant already created role for this client  
    
         begin
             oauth.grant_client_role(
@@ -32,17 +32,18 @@ Fist thing first
         commit;
         end;
 
-7. Now check your CLEINT_ID and CLEINT_SECRET for newly created client
+--7. Now check your CLEINT_ID and CLEINT_SECRET for newly created client
 
         select * from user_ords_clients;
 
-8. Now you can check your API using POSTMAN, IF its Response 200 (OK) then your API is publicly accessible.
-To prevent public access you need to go back         SQL Workshop    >   RESTful Services\Privileges\
-and then go to your created Privilege and Protect your module.
+--8. Now you can check your API using POSTMAN, IF its Response 200 (OK) then your API is publicly accessible.
+--To prevent public access you need to go back         
+SQL Workshop    >   RESTful Services\Privileges\
+--and then go to your created Privilege and Protect your module.
 
-9. Now Check again and you will get Response 401 (Unauthorized).
+--9. Now Check again and you will get Response 401 (Unauthorized).
 
-10. To access with OAuth2.0 you follow this steps below:
+--10. To access with OAuth2.0 you follow this steps below:
     a.  Go to Authorization tab
     b.  Set Type OAuth 2.0
     c.  Configure New Token
@@ -54,9 +55,10 @@ and then go to your created Privilege and Protect your module.
     i.  Client Authentication   'Send as Basic Auth Header'
     j.  Now press Get New Access Token and Use Token
 
-11. Now hit the API Link again and you will get Status 200 (OK)
+--11. Now hit the API Link again and you will get 
+        Status 200 (OK)
 
-12. If you want to access the token with Basic auth then you need to pass the username and password to access the API.
+--12. If you want to access the token with Basic auth then you need to pass the username and password to access the API.
 
     select apex_web_service.make_rest_request(p_url => 'https://apex.oracle.com/pls/apex/jasmr/EMP/ALL',
         p_http_method => 'GET',
@@ -64,7 +66,7 @@ and then go to your created Privilege and Protect your module.
         p_password => 'Suddin000') from dual;
 
 
-13. Enjoy and spread!
+--13. Enjoy and spread!
 
 
 ****For detail guideline: https://oracle-base.com/articles/misc/oracle-rest-data-services-ords-authentication
